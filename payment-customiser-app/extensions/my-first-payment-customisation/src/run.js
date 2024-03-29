4265dc28672ae87bc122983078d0365b19dc5091
munzah-shah <munzah.shah00@gmail.com>
2024-03-30 00:47:32 +0500

    Added a function that uses hard-coded values for price and payment method name

--- payment-customiser-app/extensions/my-first-payment-customisation/src/run.js
+++ payment-customiser-app/extensions/my-first-payment-customisation/src/run.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+// Use JSDoc annotations for type safety
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
@@ -17,9 +18,35 @@ const NO_CHANGES = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const configuration = JSON.parse(
-    input?.paymentCustomization?.metafield?.value ?? "{}"
+  // Get the cart total from the function input, and return early if it's below 100
+  const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0");
+  if (cartTotal < 100) {
+    // manually setting threshold price for now
+    console.error(
+      // this will be displayed under STDERR in function logs on Partners Dashboard
+      "Cart total is not high enough, no need to hide the payment method."
+    );
+    return NO_CHANGES;
+  }
+
+  // Find the payment method to hide
+  const hidePaymentMethod = input.paymentMethods.find(
+    (method) => method.name.includes("Cash on Delivery") // adding the method manuually for now
   );
 
-  return NO_CHANGES;
-};
+  if (!hidePaymentMethod) {
+    return NO_CHANGES;
+  }
+
+  // The @shopify/shopify_function package applies JSON.stringify() to your function result
+  // and writes it to STDOUT
+  return {
+    operations: [
+      {
+        hide: {
+          paymentMethodId: hidePaymentMethod.id,
+        },
+      },
+    ],
+  };
+}
